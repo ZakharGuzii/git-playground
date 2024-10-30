96ca560adb9db5e8f1e0c56bd0fdd854b232a341
ZakharGuzii <zhuzii@kse.org.ua>
2024-10-30 15:53:26 +0200

    add game over message

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def congratulate_user():
 def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
 
+def game_over_message():
+    print("====================")
+    print("=  Game Over! You lost!  =")
+    print("====================")
 
 def guess_is_valid(candidate):
     if candidate in guesses:
