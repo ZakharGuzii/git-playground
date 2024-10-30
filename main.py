0788f3712ae5ac2051b01c3a80bd32996a28b73e
ZakharGuzii <zhuzii@kse.org.ua>
2024-10-30 15:52:11 +0200

    add same words chek

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 
 def congratulate_user():
-    print(f"Congratulations, you won! your words: {guesses}")
+    print(f"Congratulations, you won! Your words: {guesses}")
     print("=============================")
     print("= Congratulations! You won! =")
     print("=============================")
@@ -14,20 +14,23 @@ def is_game_over():
 
 
 def guess_is_valid(candidate):
+    if candidate in guesses:
+        print("You've already guessed this word.")
+        return False
+
     for letter in candidate:
         if letter not in word:
-            print(f"You can not use letter {letter}")
+            print(f"You cannot use letter '{letter}'")
             return False
         count = word.count(letter)
         if count < candidate.count(letter):
-            print(f"You can use letter {letter} only {count} times")
+            print(f"You can use letter '{letter}' only {count} times")
             return False
     return True
 
 
 guessed = 0
 errors = 0
-
 guesses = []
 
 WORDS_TO_WIN = 5
@@ -37,12 +40,11 @@ words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
 
-print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
+print(f"Can you make up {WORDS_TO_WIN} words from letters in the word provided by me?")
 print(f"Your word is '{word}'")
 
-
 while not is_game_over():
-    guess = input("Your next take: ")
+    guess = input("Your next take: ").lower()
 
     if not guess_is_valid(guess):
         continue
@@ -56,4 +58,8 @@ while not is_game_over():
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
     else:
         errors += 1
-        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives left")
+
+if errors == ERRORS_TO_LOSE:
+    game_over_message()
+
